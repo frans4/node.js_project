5a29a897072c1dd4d2c18a2b9093883d28ce1ec9
Gate Keeper <frans49@mail.broward.edu>
2016-09-16 15:18:51 -0400

    added a function based on times variable.

--- index.js
+++ index.js
@@ -17,4 +17,11 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
-
+//repeats an action depending on the value of the TIMES environment variable
+app.get('/times', function(request, response) {
+    var result = ''
+    var times = process.env.TIMES || 5
+    for (i=0; i < times; i++)
+      result += i + ' ';
+  response.send(result);
+});
